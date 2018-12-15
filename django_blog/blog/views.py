aac2546568aa183446a5a059e927697feb933341
RazaHussain090 <razahussain090@gmail.com>
2018-12-15 22:38:17 +0500

    updated

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Raza',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Hussain',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog home </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> About Blog </h1>')
+    return render(request,'blog/about.html')
